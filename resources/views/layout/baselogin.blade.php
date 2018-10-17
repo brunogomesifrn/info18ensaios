1cc97a2a3b6cbc6a4f44afad0a43f2f95f90f762
Luiz Matheus Dias Dantas <matheus.dantas@academico.ifrn.edu.br>
2018-10-17 15:57:05 -0300

    criando a base, a extensão e o yield

---
+++ resources/views/layout/baselogin.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<title>Baboon Photos</title>
+
+	<link href="css_l/style.css" rel="stylesheet">
+	<link href="color_l/default.css" rel="stylesheet">
+	<script type="text/javascript">
+
+
+	</script>
+
+</head>
+
+<body>
+
+	<!-- navbar -->
+
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<!-- Responsive navbar -->
+
+					<h1 class="brand"=>BABOON PHOTOS</h1>
+					<!-- navegação -->
+					<nav class="pull-right nav-collapse collapse">
+						<ul id="menu-main" class="nav">
+							<li><a href="telacadastro.html" target="_self">Criar nova conta</a></li>
+
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+
+@yield('conteudo')
+
+
+</body>
+
+</html>
--- resources/views/teladelogin.blade.php
+++ resources/views/teladelogin.blade.php
@@ -1,42 +1,7 @@
-<!DOCTYPE HTML>
-<html lang="pt-br">
 
-<head>
-	<meta charset="utf-8">
-	<title>Baboon Photos</title>
-
-	<link href="css_l/style.css" rel="stylesheet">
-	<link href="color_l/default.css" rel="stylesheet">
-	<script type="text/javascript">
-
-
-	</script>
-
-</head>
-
-<body>
-
-	<!-- navbar -->
-
-		<div class="navbar navbar-inverse navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container">
-					<!-- Responsive navbar -->
-
-					<h1 class="brand"=>BABOON PHOTOS</h1>
-					<!-- navegação -->
-					<nav class="pull-right nav-collapse collapse">
-						<ul id="menu-main" class="nav">
-							<li><a href="telacadastro.html" target="_self">Criar nova conta</a></li>
-
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</div>
-
+@extends('baselogin.base')	
 	<!-- Header area -->
-
+@section('conteudo')
 	<section id="login" class="section green">
 		<div class="container">
 
@@ -57,6 +22,4 @@
 	</section>
 	<!-- end section: services -->
 
-</body>
-
-</html>
+@endsection
