1c29a22b0ffd6f1e250e93edf95befba04bacea6
Alisson Rodrigues Soares da Silva <alissonrodrigues8519@gmail.com>
2018-10-17 15:42:05 -0300

    Adicionando tela de login na view

---
+++ resources/views/teladelogin.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<title>Baboon Photos</title>
+
+	<link href="css_l/style.css" rel="stylesheet">
+	<link href="color_l/default.css" rel="stylesheet">
+	<script type="text/javascript">
+
+
+	</script>
+
+</head>
+
+<body>
+
+	<!-- navbar -->
+
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<!-- Responsive navbar -->
+
+					<h1 class="brand"=>BABOON PHOTOS</h1>
+					<!-- navegação -->
+					<nav class="pull-right nav-collapse collapse">
+						<ul id="menu-main" class="nav">
+							<li><a href="telacadastro.html" target="_self">Criar nova conta</a></li>
+
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+
+	<!-- Header area -->
+
+	<section id="login" class="section green">
+		<div class="container">
+
+			<!-- Four columns -->
+			 <div id="countdown" ></div>
+		  <div class="subcription-info text-center">
+		  <h4>Faça seu login</h4>
+          <form class="subscribe_form" action="telalogin.php" method="post">
+            <input required="" value="" placeholder="Digite seu email" class="email" id="email" name="email" type="email"> <br/>
+            <input required="" value="" placeholder="Digite sua senha" class="email" id="email" name="senha" type="password"> <br/>
+            <input type="submit"  value="Entrar">
+          </form>
+          <a href="https://bootstrapmade.com/">Esqueceu sua senha?</a>
+        </div>
+      </div>
+    </div>
+		</div>
+	</section>
+	<!-- end section: services -->
+
+</body>
+
+</html>
