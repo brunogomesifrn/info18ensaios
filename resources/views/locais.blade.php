23cfe0714ac3db04308eece008a9d0226a4a5eb9
Luiz Matheus Dias Dantas <matheus.dantas@academico.ifrn.edu.br>
2018-12-17 14:30:49 -0300

    melhorando layout da tela do usuario

--- resources/views/locais.blade.php
+++ resources/views/locais.blade.php
@@ -187,7 +187,7 @@
 			<div class="footer-grids">
 				<div class="col-md-3 footer-grid animated wow slideInLeft" data-wow-delay=".5s">
 					<h3>Sobre nós</h3>
-					<p>Desenvolvedores: Anderson Freitas, Luiz Matheus e Wanderleia Klécia. <span>Para qualquer dúvida, entre em contato conosco.</span></p>
+					<p>Desenvolvedores: Alisson Rodrigues, Anderson Freitas, Luiz Matheus e Wanderleia Klécia. <span>Para qualquer dúvida, entre em contato conosco.</span></p>
 				</div>
 				<div class="col-md-3 footer-grid animated wow slideInLeft" data-wow-delay=".6s">
 					<h3>Contatos</h3>
